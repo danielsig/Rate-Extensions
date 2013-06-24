85912f368dfddbb9431699fcdba5fb3b33f49533
danielsig <danielsig@mac-pro.fritz.box>
2013-06-24 22:52:42 +0000

    Basic API caller + Ajax post and response
    Upon clicking the extension's icon it gathers data about installed
    extensions, then sends that data as a json object to a webservice,
    using Ajax.

--- js/background.js
+++ js/background.js
@@ -1,1 +1,31 @@
-alert("hello world");
+function iconClicked()
+{
+	chrome.management.getAll( apiCallback );
+}
+
+function apiCallback(extensions)
+{
+	console.log("Sending first 3 extensions...");
+	console.log( postAjax( [extensions[0], extensions[1], extensions[2]], webServiceCallback) );
+	console.log("Done sending!");
+}
+function webServiceCallback(response)
+{
+	console.log("Response: " + response);
+}
+function postAjax(json, handler) {
+	var request =
+	{
+  		type: "POST"
+  		,url: 'http://google.com/'
+  		,data: {
+  			"json": json
+  			,success: handler
+  		}
+	};
+	$.ajax( request );
+	return request;
+}
+
+
+chrome.browserAction.onClicked.addListener( iconClicked );
